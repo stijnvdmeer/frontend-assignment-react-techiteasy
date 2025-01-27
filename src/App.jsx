c70366420ffaaa22cf97d484571c0518a233dbfa
stijnvdmeer <stijn08072002@outlook.com>
2025-01-27 11:38:37 +0100

    opdracht 2b Created and implemented helper function formatPriceString

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './styles/App.css';
+import {bestSellingTv} from "./constants/inventory.js";
 
 import SalesOverview from "./components/SalesOverview.jsx";
 
@@ -6,6 +7,7 @@ import calculateSoldProducts from "./helperFuntions/calculateSoldProducts.js";
 import calculateOrderedProducts from "./helperFuntions/calculateOrderedProducts.js";
 import calculateRemnantProducts from "./helperFuntions/calculateRemnantProducts.js";
 import getProductTitleString from "./helperFuntions/getProductTitleString.js";
+import formatPriceString from "./helperFuntions/formatPriceString.js";
 
 function App() {
   return (
@@ -24,7 +26,7 @@ function App() {
               <img src="https://image.coolblue.nl/max/500x500/products/1786196" alt="picture of computer monitor"/>
               <div className="bestSoldArticle-inner">
                   <h2>{getProductTitleString()}</h2>
-                  <h2>$549</h2>
+                  <h2>{formatPriceString(bestSellingTv.price)}</h2>
                   <h2>43, 50, 55, 65</h2>
               </div>
           </article>
---
+++ src/helperFuntions/formatPriceString.js
@@ -0,0 +1,3 @@
+export default function (price) {
+    return `€${price},-`;
+}
