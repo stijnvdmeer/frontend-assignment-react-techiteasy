93593e7481a778082a84bc9a35d2e1d268a5496b
stijnvdmeer <stijn08072002@outlook.com>
2025-01-27 09:49:07 +0100

    added stylesheet for the SalesOverview component and added import to SalesOverview Component

--- src/components/SalesOverview.jsx
+++ src/components/SalesOverview.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
 
+import '../styles/salesOverview.css'
+
 export default function SalesOverview(props) {
     return (
-        <article>
+        <article className={props.classes} id={props.id}>
             <p>{props.title}</p>
             <h2>{props.value}</h2>
         </article>
