6b511c08b9cad885301d4ff09c8eb3593f3429db
stijnvdmeer <stijn08072002@outlook.com>
2025-01-27 09:48:20 +0100

    added outer and inner container + styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import SalesOverview from "./components/SalesOverview.jsx";
 
 function App() {
   return (
-      <>
-
+      <main className="outer-container">
+        <div className="inner-container">
           <h1>Tech It Easy Dashboard</h1>
           <h2>Verkoopoverzicht</h2>
-          <SalesOverview title="Aantal verkochte producten" value="13" classes="salesoverview" id="salesoverview-soldproducts" />
-          <SalesOverview title="Aantal ingekochte producten" value="13" classes="salesoverview" id="salesoverview-orderedproducts" />
-          <SalesOverview title="Aantal te verkopen producten" value="13" classes="salesoverview" id="salesoverview-remnantproducts" />
-          <h2>Best verkochte tv</h2>d
+          <div className="salesOverviews">
+              <SalesOverview title="Aantal verkochte producten" value="13" classes="salesoverview" id="salesoverview-soldproducts" />
+              <SalesOverview title="Aantal ingekochte producten" value="13" classes="salesoverview" id="salesoverview-orderedproducts" />
+              <SalesOverview title="Aantal te verkopen producten" value="13" classes="salesoverview" id="salesoverview-remnantproducts" />
+          </div>
+
+          <h2>Best verkochte tv</h2>
           <article></article>
-      </>
+        </div>
+      </main>
   )
 }
 
