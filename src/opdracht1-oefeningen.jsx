0cbece1464bfddf703726f98377ec5fe293b139e
stijnvdmeer <stijn08072002@outlook.com>
2025-01-31 11:34:11 +0100

    part 2 opdracht 1c

--- src/opdracht1-oefeningen.jsx
+++ src/opdracht1-oefeningen.jsx
@@ -3,11 +3,17 @@ import {inventory} from './constants/inventory.js'
 // Opdracht 1
 const arrNames = inventory.map(item => {
     return item.name;
-})
+});
 console.log(arrNames);
 
 // Opdracht 2
 const soldOutTVs = inventory.filter((tv) => {
     return tv.originalStock - tv.sold <= 0;
-})
+});
 console.log(soldOutTVs);
+
+// Opdracht 3
+const selTV = inventory.find((tv) => {
+    return tv.type === "NH3216SMART";
+});
+console.log(selTV);
