bcc7c740ab6f4e4debe88a7722e5a7ddfff3d124
stijnvdmeer <stijn08072002@outlook.com>
2025-01-31 14:21:11 +0100

    part 2 added icon component and styles

---
+++ src/components/Icon.jsx
@@ -0,0 +1,14 @@
+import '../styles/icon.css';
+
+import check from '../assets/check.png'
+import minus from '../assets/minus.png'
+
+export default function Icon (props) {
+
+    const type = (props.iconType === true ? check : minus )
+    const alt = (props.iconType === true ? "yes" : "no" )
+
+    return (
+      <img className="icons" src={type} alt={alt} />
+    );
+}
