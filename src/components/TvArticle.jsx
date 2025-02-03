aa3c6ca18a97ccfa77336caf2a029126df5ebc05
stijnvdmeer <stijn08072002@outlook.com>
2025-02-03 14:48:26 +0100

    BONUS sold out picture implemented in TvArticle

--- src/components/TvArticle.jsx
+++ src/components/TvArticle.jsx
@@ -1,16 +1,19 @@
 import '../styles/tvarticle.css';
+import soldOut from '../assets/out-of-stock.png';
+
 import Icon from '../components/Icon';
 
 import getProductTitleString from "../helperFunctions/getProductTitleString.js";
 import formatPriceString from "../helperFunctions/formatPriceString.js";
 import getProductSizesString from "../helperFunctions/getProductSizesString.js";
+import calculateRemnantProduct from "../helperFunctions/calculateRemnantProducts.js";
 
 
 
 export default function TvArticle(props) {
     return (
         <article className="tvArticle-outer">
-            <img src={props.tv.sourceImg} alt="tv" />
+            <img src={calculateRemnantProduct(props.tv) === 0 ?  soldOut : props.tv.sourceImg} alt="tv" />
             <div className="tvArticle-inner">
                 <h2>{getProductTitleString(props.tv)}</h2>
                 <h2>{formatPriceString(props.tv)}</h2>
